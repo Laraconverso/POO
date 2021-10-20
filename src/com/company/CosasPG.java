12a8d046728caaa0f43f16ffe238d3b33ae4bb6f
Lara Converso <laraconverso@gmail.com>
2021-10-20 00:05:55 -0300

    otro commit

--- src/com/company/CosasPG.java
+++ src/com/company/CosasPG.java
@@ -1,4 +1,20 @@
 package com.company;
 
+import java.util.Locale;
+
 public class CosasPG {
+    public static void main(String[] args) {
+        String name = "Jhon";
+        int quantity;
+        char inicial;
+
+        quantity = name.length();
+
+        name.toUpperCase();
+        if(name.equals("JHON")){
+            System.out.println("Se paso a mayuscula");
+        } else { System.out.println("No se paso a mayuscula");}
+        inicial = name.charAt(0);
+        System.out.println(quantity + " " + inicial);
+    }
 }
