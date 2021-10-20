6b556f7818fc22f8d0fc59e8c5f0da5f1a399cf3
Lara Converso <laraconverso@gmail.com>
2021-10-19 23:46:55 -0300

    first commit

---
+++ src/com/company/C1S.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class C1S {
+
+    static boolean esDivisible(int n, int divisor){
+        if ((n % divisor) == 0) {
+            System.out.println("TRUE.");
+            return true;
+        } else {
+            System.out.println("FALSE.");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        // ej de structures//
+        int number1 = 1;
+        int number2 = 2;
+        if(number1 % number2 == 0){
+            System.out.println("El number es divisible");
+        } else {
+            System.out.println("El number no es divisible");
+        }
+        //ej de functions//
+        esDivisible(number1, number2);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello Java World!");
+        int num = 10;
+        System.out.println("El valor es " + num);
+
+
+    }
+}
