f7064c36b27d5545936b4ebe7a59e94a58132494
Lara Converso <laraconverso@gmail.com>
2021-10-20 13:15:50 -0300

    add ej

--- src/com/company/CosasPG.java
+++ src/com/company/CosasPG.java
@@ -1,9 +1,11 @@
 package com.company;
 
 import java.util.Locale;
+import java.util.Scanner;
 
 public class CosasPG {
-    public static void main(String[] args) {
+    public static <string> void main(String[] args) {
+        /*
         String name = "Jhon";
         int quantity;
         char inicial;
@@ -16,5 +18,24 @@ public class CosasPG {
         } else { System.out.println("No se paso a mayuscula");}
         inicial = name.charAt(0);
         System.out.println(quantity + " " + inicial);
+
+         */
+        Scanner lector;
+        lector = new Scanner(System.in);
+        int num1;
+        int num2;
+        float coef;
+        String nombre;
+        char inicial;
+        System.out.println("Ingrese 1 valor");
+        num1 = lector.nextInt();
+        System.out.println("Ingrese segundo valor");
+        num2 = lector.nextInt();
+        System.out.println("Ingrese coef");
+        coef = lector.nextFloat();
+        System.out.println("Ingrese nombre");
+        nombre = lector.next();
+        inicial= nombre.charAt(0);
+        System.out.println("Tu inicial es " + inicial + ".");
     }
 }
